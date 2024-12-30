02c2889ad35768ef54036e101b0faa00c510588f
FabiCerchi <angelfcerchi@gmail.com>
2024-12-30 18:53:37 +0100

    [FIX]: password not null

--- app/models/user.py
+++ app/models/user.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     email = Column(String(50), unique=True, nullable=False, index=True)
     username = Column(String(24), unique=True, nullable=False, index=True)
-    password = Column(String(64))
+    password = Column(String(64), nullable=False)
     address = Column(String, nullable=True)
     created_at = Column(DateTime, default=datetime.now())
     updated_at = Column(DateTime, default=datetime.now(), onupdate=datetime.now())
