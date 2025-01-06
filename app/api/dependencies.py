b2844c4ce59771c5551c111ef72e3c5059248989
FabiCerchi <angelfcerchi@gmail.com>
2025-01-06 12:45:17 +0100

    [ADD]: Se agrega validacion si existe el current_user

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -5,7 +5,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
-
+from app.schemas.token import TokenData
 from app.utils.oauth import OAuth
 from app.utils.token import Token
 from app.db.database import get_db
@@ -20,18 +20,41 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 # Instancia de OAuth
 oauth_service = OAuth(Token())
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> TokenData:
     """
     Función que inyecta la dependencia para los endpoints.
     """
     try:
-        return oauth_service.get_current_user(token)
+        # Decodificar el token y obtener datos del usuario actual
+        current_user: TokenData = oauth_service.get_current_user(token)
+
+        # Validar que los datos básicos del usuario existen en el token
+        if not current_user.id or not current_user.username:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token data",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        # Verificar si el usuario existe en la base de datos
+        user_service = get_user_service(db)
+        if not user_service.get_user_by_id(current_user.id):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+
+        return current_user
+
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=str(e),
             headers={"WWW-Authenticate": "Bearer"},
         )
+
 
 def get_user_service(db: Session = Depends(get_db)) -> UserService:
     """
@@ -41,6 +64,7 @@ def get_user_service(db: Session = Depends(get_db)) -> UserService:
     """
     repo = UserRepository(db)
     return UserService(repo)
+
 
 def get_auth_service(db: Session = Depends(get_db)) -> AuthService:
     """
